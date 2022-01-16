4d387368c271a2e62ca5e494ca56dfe4add63f0f
kartikmehra1998 <77176107+kartikmehra1998@users.noreply.github.com>
2022-01-16 14:38:22 +0530

    Add files via upload

---
+++ js/main_script.js
@@ -0,0 +1,116 @@
+{
+    // browse location
+    let location_item = [
+        {
+            background_img : "images/Dev/Browse_by_location/Banglore.jpg",
+            city : "Banglore"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Gurugram.jpg",
+            city : "Gurugram"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Delhi.jpg",
+            city : "Delhi"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Hyderabad.jpg",
+            city : "Hyderabad"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Jaipur.jpg",
+            city : "Jaipur"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Kolkata.jpg",
+            city : "Kolkata"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Noida.jpg",
+            city : "Noida"
+        },
+        {
+            background_img : "images/Dev/Browse_by_location/Quote.jpg",
+            city : ""
+        }
+    ];
+
+    let location_cont = document.querySelector('#location-container ul')
+
+    // adding elemnts in the list
+    location_item.forEach(function(item){
+        let city_list = document.createElement('li');
+        city_list.style.backgroundImage = "url('"+item.background_img+"')";
+        let city_div = document.createElement('div');
+        city_div.className = "city-div";
+        let city_name = document.createElement('span');
+        city_name.innerText = item.city + " ";
+        city_div.appendChild(city_name);
+        city_list.appendChild(city_div);
+        location_cont.appendChild(city_list);
+    });
+
+
+    // features
+    let features_item = [
+        {
+            image : "images/Dev/Why Olive/1.jpg",
+            content : "Private non-sharing apartments"
+        },
+        {
+            image : "images/Dev/Why Olive/2.jpg",
+            content : "Fully serviced apartments"
+        },
+        {
+            image : "images/Dev/Why Olive/3.jpg",
+            content : "Low rates top savings"
+        },
+        {
+            image : "images/Dev/Why Olive/4.jpg",
+            content : "Reviewd by real traveller"
+        },
+        {
+            image : "images/Dev/Why Olive/5.jpg",
+            content : "Value for money"
+        },
+        {
+            image : "images/Dev/Why Olive/6.jpg",
+            content : "We are secure"
+        },
+        {
+            image : "images/Dev/Why Olive/7.jpg",
+            content : "Enjoy more space"
+        },
+        {
+            image : "images/Dev/Why Olive/8.jpg",
+            content : "Live like a local"
+        },
+        {
+            image : "images/Dev/Why Olive/9.jpg",
+            content : "Feel at home"
+        },
+        {
+            image : "images/Dev/Why Olive/10.jpg",
+            content : "More choices"
+        }
+    ];
+
+
+    let feature_list = document.querySelector('#features ul');
+
+
+    features_item.forEach(function(item){
+
+        let feature_li = document.createElement('li');
+        let div1 = document.createElement('div');
+        let feature_img = document.createElement('img');
+        feature_img.src = item.image;
+        div1.appendChild(feature_img);
+        let div2 = document.createElement('div');
+        div2.textContent = item.content;
+        feature_li.appendChild(div1);
+        feature_li.appendChild(div2);
+        feature_list.appendChild(feature_li);
+    })
+
+}
